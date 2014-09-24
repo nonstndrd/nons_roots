ca52c6322ae3288df51763cc798d3e1cef56901a
nonstndrd <kwasi.b@nonstndrd.com>
2014-09-24 16:10:57 -0700

    edits

--- base.php
+++ base.php
@@ -14,14 +14,14 @@
 
   <div class="wrap container" role="document">
     <div class="content row">
-      <main class="main" role="main">
-        <?php include roots_template_path(); ?>
-      </main><!-- /.main -->
       <?php if (roots_display_sidebar()) : ?>
         <aside class="sidebar" role="complementary">
           <?php include roots_sidebar_path(); ?>
         </aside><!-- /.sidebar -->
       <?php endif; ?>
+      <main class="main" role="main">
+        <?php include roots_template_path(); ?>
+      </main><!-- /.main -->
     </div><!-- /.content -->
   </div><!-- /.wrap -->
 
