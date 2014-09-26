3067ba4d0e9e726f4b260861bc1b240d10afae91
nonstndrd <kwasi.b@nonstndrd.com>
2014-09-26 13:52:42 -0700

    edits

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,17 @@
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
+
   <div class="container">
-    <div class="navbar-header">
+  <div class="logo"><img src="/Wordpress/wp-content/themes/nons_roots/assets/img/CITL-web.png">    <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      </button> 
     </div>
 
+</div>
+  <div class="all-nav">
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
@@ -17,5 +19,6 @@
         endif;
       ?>
     </nav>
+    </div>
   </div>
 </header>
